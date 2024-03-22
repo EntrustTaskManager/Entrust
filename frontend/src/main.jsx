d96b81afd7a9b1f86d6cac53413700d50d0e38a1
Marquez Noel <74875633+QzyKuma@users.noreply.github.com>
2024-03-21 21:04:32 -0400

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,20 +1,8 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import Login from "../src/components/Login.jsx"; // Import your Login component
-import { BrowserRouter } from "react-router-dom";
-import "./index.css";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
 
-// Check if the user is logged in or not
-const isLoggedIn = localStorage.getItem("token");
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      {/* Render the Login component if the user is not logged in */}
-      {!isLoggedIn && <Login />}
-      {/* Render the App component if the user is logged in */}
-      {isLoggedIn && <App />}
-    </BrowserRouter>
-  </React.StrictMode>
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <App />
 );
