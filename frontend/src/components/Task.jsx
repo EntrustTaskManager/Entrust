02f918aab69c04281e9ea2c2a810d04a211a390a
Marquez Noel <74875633+QzyKuma@users.noreply.github.com>
2024-03-21 21:03:19 -0400

    Update Task.jsx

--- frontend/src/components/Task.jsx
+++ frontend/src/components/Task.jsx
@@ -2,22 +2,15 @@ import React from "react";
 
 import AddTask from "./AddTask";
 
-import TasksContainer from "./TasksContainer";
+import TasksContainer from "./TaskContainer";
 
 import Nav from "./Nav";
 
 import socketIO from "socket.io-client";
 
 
-/*
 
-👇🏻  Pass Socket.io into the required components
-
-    where communications are made with the server
-
-*/
-
-const socket = socketIO.connect("http://localhost:5173/");
+const socket = socketIO.connect("http://localhost:4000");
 
 
 const Task = () => {
