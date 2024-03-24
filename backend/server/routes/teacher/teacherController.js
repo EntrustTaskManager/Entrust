77a2071f00387b62d541717093a883e875d368c0
John Fuller <fuller.john84@gmail.com>
2024-03-24 11:51:10 -0400

    updated server and routes for tasks, teachers, and students. Organized route folder structure.

---
+++ backend/server/routes/teacher/teacherController.js
@@ -0,0 +1,84 @@
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
+
+async function createTeacher(req, res) {
+  try {
+    const newTeacher = await prisma.teacher.create({ data: req.body });
+    res.json(newTeacher);
+  } catch (error) {
+    // Handle the error
+    console.error("Error creating teacher:", error);
+    res.status(500).json({ error: "Failed to create teacher" });
+  }
+}
+
+async function getAllTeachers(req, res) {
+  try {
+    const allTeachers = await prisma.teacher.findMany();
+    res.json(allTeachers);
+  } catch (error) {
+    // Handle the error
+    console.error("Error retrieving teachers:", error);
+    res.status(500).json({ error: "Failed to retrieve teachers" });
+  }
+}
+
+async function getTeacherById(req, res) {
+  const { uuid } = req.params;
+
+  try {
+    const teacher = await prisma.teacher.findUnique({
+      where: {
+        id: uuid,
+      },
+    });
+
+    if (!teacher) {
+      return res.status(404).json({ error: "Teacher not found" });
+    }
+
+    res.json(teacher);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Failed to retrieve teacher" });
+  }
+}
+
+async function updateTeacher(req, res) {
+  const { uuid } = req.params;
+  const { firstName, lastName, username, password, email } = req.body;
+
+  try {
+    const updatedTeacher = await prisma.teacher.update({
+      where: { id: uuid },
+      data: { firstName, lastName, username, password, email },
+    });
+    res.json(updatedTeacher);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Failed to update teacher" });
+  }
+}
+
+async function deleteTeacher(req, res) {
+  const { uuid } = req.params;
+
+  try {
+    const deletedTeacher = await prisma.teacher.delete({
+      where: { id: uuid },
+    });
+
+    res.json(deletedTeacher);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Failed to delete teacher" });
+  }
+}
+
+module.exports = {
+  createTeacher,
+  getAllTeachers,
+  getTeacherById,
+  updateTeacher,
+  deleteTeacher,
+};
