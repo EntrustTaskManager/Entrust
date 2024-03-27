5c0d63048454c211dd8590b7af6cf2702822ec18
Marquez Noel <74875633+QzyKuma@users.noreply.github.com>
2024-03-27 18:52:12 -0400

    Update Nav.jsx

--- frontend/src/components/Nav.jsx
+++ frontend/src/components/Nav.jsx
@@ -1,15 +1,21 @@
 import React from "react";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+
+
 
 
 const Nav = () => {
 
     return (
-
-        <nav className='navbar'>
-
-            <h3>Entrust</h3>
-
-        </nav>
+        <AppBar position='static'>
+            <Toolbar sx={{ minHeight: '48px'}}>
+                <Typography vatiant='h3' component='div' sx={{flexGrow: 1, textAlign: 'center', fontSize: '30px'}}>
+                    Entrust
+                </Typography>
+            </Toolbar>
+        </AppBar>
 
     );
 
