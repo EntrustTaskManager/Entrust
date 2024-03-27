eac623a26682d8e059e7cc803a4a4154d35857db
John Fuller <fuller.john84@gmail.com>
2024-03-27 19:54:13 -0400

    updated file structure

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,30 +1,26 @@
-import { useState } from 'react'
+import { useState } from "react";
 import { ChakraProvider, theme, Text } from "@chakra-ui/react";
-import KanbanBoard  from "./components/KanbanBoard.jsx";
+import KanbanBoard from "./components/KanbanBoard.jsx";
 import Login from "./components/Login";
-import './App.css'
-import {BrowserRouter, Route, Routes} from "react-router-dom";
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-      <ChakraProvider theme={theme}>
-          <BrowserRouter>
+    <ChakraProvider theme={theme}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login />} />
 
-              <Routes>
+          <Route path="/KanbanBoard" element={<KanbanBoard />} />
 
-                  <Route path='/' element={<Login />} />
-
-                  <Route path='/KanbanBoard' element={<KanbanBoard />} />
-
-                  {/*<Route path='/comments/:category/:id' element={<Comments />} />*/}
-
-              </Routes>
-
-          </BrowserRouter>
-      </ChakraProvider>
-  )
+          {/*<Route path='/comments/:category/:id' element={<Comments />} />*/}
+        </Routes>
+      </BrowserRouter>
+    </ChakraProvider>
+  );
 }
 
-export default App
+export default App;
--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,97 +1,107 @@
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import {
-    Box,
-    Button,
-    Input,
-    VStack,
-    Text,
-    Flex,
-    useColorModeValue,
-    Image
-} from '@chakra-ui/react';
+  Box,
+  Button,
+  Input,
+  VStack,
+  Text,
+  Flex,
+  useColorModeValue,
+  Image,
+} from "@chakra-ui/react";
+import axios from "axios";
 
 const Login = () => {
-    const [username, setUsername] = useState("");
-    const [password, setPassword] = useState("");
-    const navigate = useNavigate();
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  //   const navigate = useNavigate();
 
-    const handleLogin = (e) => {
-        e.preventDefault();
-        // Authentication logic here
-        localStorage.setItem("userId", username);
-        setUsername("");
-        setPassword("");
-        navigate("/KanbanBoard");
-    };
+  //   const handleLogin = (e) => {
+  //     e.preventDefault();
+  //     // Authentication logic here
+  //     localStorage.setItem("userId", username);
+  //     setUsername("");
+  //     setPassword("");
+  //     navigate("/KanbanBoard");
+  //   };
 
-    const formBackground = useColorModeValue("gray.100", "gray.700");
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post("/login", {
+        username,
+        password,
+      });
 
-    return (
-        <Flex
-            align="center"
-            justify="center"
-            height="100vh"
-            bg={useColorModeValue("gray.50", "gray.800")}
-        >
-            <Box
-                d="flex"
-                boxShadow="2xl"
-                borderRadius="2xl"
-                overflow="hidden"
-                maxWidth="900px"
-                width="full"
-                bg={formBackground}
-            >
-                <Flex direction="row" width="full" align="center">
-                    <Image
-                        src="https://www.thoughtco.com/thmb/mi5BR5mruiShD9HnmFwoIu241E4=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/high-school-teacher-calling-on-student-in-classroom-595349163-5adf35e6fa6bcc0036b16732.jpg"
-                        alt="Login Image"
-                        objectFit="cover"
-                        width="50%"
-                        borderRadius="2xl"
-                    />
+      const token = response.data.token;
 
-                    <VStack
-                        align="center"
-                        justify="center"
-                        p={8}
-                        spacing={6}
-                        width="50%"
-                    >
-                        <Text fontSize="3xl" fontWeight="bold">Entrust</Text>
-                        <Box as="form" onSubmit={handleLogin} width="full">
-                            <VStack spacing={5}>
-                                <Input
-                                    placeholder="Username"
-                                    size="lg"
-                                    bg="white"
-                                    value={username}
-                                    onChange={(e) => setUsername(e.target.value)}
-                                />
-                                <Input
-                                    placeholder="Password"
-                                    size="lg"
-                                    bg="white"
-                                    type="password"
-                                    value={password}
-                                    onChange={(e) => setPassword(e.target.value)}
-                                />
-                                <Button
-                                    width="full"
-                                    size="lg"
-                                    colorScheme="blue"
-                                    type="submit"
-                                >
-                                    Log In
-                                </Button>
-                            </VStack>
-                        </Box>
-                    </VStack>
-                </Flex>
+      localStorage.setItem("token", token);
+
+      Navigate("/KanbanBoard");
+    } catch (error) {
+      console.error("Authentication failed:", error);
+    }
+  };
+
+  const formBackground = useColorModeValue("gray.100", "gray.700");
+
+  return (
+    <Flex
+      align="center"
+      justify="center"
+      height="100vh"
+      bg={useColorModeValue("gray.50", "gray.800")}
+    >
+      <Box
+        d="flex"
+        boxShadow="2xl"
+        borderRadius="2xl"
+        overflow="hidden"
+        maxWidth="900px"
+        width="full"
+        bg={formBackground}
+      >
+        <Flex direction="row" width="full" align="center">
+          <Image
+            src="https://www.thoughtco.com/thmb/mi5BR5mruiShD9HnmFwoIu241E4=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/high-school-teacher-calling-on-student-in-classroom-595349163-5adf35e6fa6bcc0036b16732.jpg"
+            alt="Login Image"
+            objectFit="cover"
+            width="50%"
+            borderRadius="2xl"
+          />
+
+          <VStack align="center" justify="center" p={8} spacing={6} width="50%">
+            <Text fontSize="3xl" fontWeight="bold">
+              Entrust
+            </Text>
+            <Box as="form" onSubmit={handleLogin} width="full">
+              <VStack spacing={5}>
+                <Input
+                  placeholder="Username"
+                  size="lg"
+                  bg="white"
+                  value={username}
+                  onChange={(e) => setUsername(e.target.value)}
+                />
+                <Input
+                  placeholder="Password"
+                  size="lg"
+                  bg="white"
+                  type="password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+                <Button width="full" size="lg" colorScheme="blue" type="submit">
+                  Log In
+                </Button>
+              </VStack>
             </Box>
+          </VStack>
         </Flex>
-    );
+      </Box>
+    </Flex>
+  );
 };
 
 export default Login;
