037ad7807ff305e0a0c9a280ba497c4d5917a2e9
Marquez Noel <74875633+QzyKuma@users.noreply.github.com>
2024-04-10 19:36:27 -0400

    Update App.jsx
    Adding in the route for admin.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,26 +1,34 @@
-import { useState } from "react";
+import { useState } from 'react'
 import { ChakraProvider, theme, Text } from "@chakra-ui/react";
-import KanbanBoard from "./components/KanbanBoard";
+import KanbanBoard  from "./components/KanbanBoard.jsx";
 import Login from "./components/Login";
-import "./App.css";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import './App.css'
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Settings from './components/Settings';
+import AdminDashboard from "./components/AdminDashboard.jsx";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState(0)
 
   return (
-    <ChakraProvider theme={theme}>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Login />} />
+      <ChakraProvider theme={theme}>
+          <BrowserRouter>
 
-          <Route path="/KanbanBoard" element={<KanbanBoard />} />
+              <Routes>
 
-          {/*<Route path='/comments/:category/:id' element={<Comments />} />*/}
-        </Routes>
-      </BrowserRouter>
-    </ChakraProvider>
+                  <Route path='/' element={<Login />} />
+
+                  <Route path='/KanbanBoard' element={<KanbanBoard />} />
+
+                  <Route path="/settings" element={<Settings />} />
+
+                  <Route path="/admin" element={<AdminDashboard />} />
+
+              </Routes>
+
+          </BrowserRouter>
+      </ChakraProvider>
   );
 }
 
-export default App;
+export default App
