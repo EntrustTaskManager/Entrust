8ba2651e70c04d5ad6d15ab042cd7297e50b565f
Marquez Noel <74875633+QzyKuma@users.noreply.github.com>
2024-04-10 20:21:57 -0400

    Update index.js
    adding in code for the Live Chat app and alt server code.

--- backend/server/index.js
+++ backend/server/index.js
@@ -5,6 +5,7 @@ const cors = require("cors");
 require("dotenv").config();
 
 const PORT = process.env.PORT;
+// const PORT = process.env.PORT || 4000;
 
 // Import route files
 const studentRoutes = require("./routes/student/studentRoutes");
@@ -22,6 +23,10 @@ app.use(
   })
 );
 
+// app.use(cors({
+//     origin: '*'
+// }));
+
 // Use route files
 app.use("/students", studentRoutes);
 app.use("/teachers", teacherRoutes);
@@ -29,3 +34,55 @@ app.use("/tasks", taskRoutes);
 app.use("/login", loginRoutes);
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+
+
+// const express = require('express');
+// const http = require('http');
+// const { Server } = require("socket.io");
+// const socketIo = require('socket.io');
+// const cors = require('cors');
+// const taskRouter = require("../Server/API/api")
+
+
+// const app = express();
+// const server = http.createServer(app);
+
+// app.use(cors({
+//     origin: '*'
+// }));
+
+// app.use(taskRouter);
+
+
+// const io = new Server(server, {
+//     cors: {
+//         origin: "http://localhost:5173",
+//         methods: ["GET", "POST", "PUT", "DELETE"]
+//     }
+// });
+
+// const PORT = process.env.PORT || 4000;
+
+// io.on('connection', (socket) => {
+//     console.log('New client connected');
+
+
+//     socket.on('message', (message) => {
+//         console.log('Message received: ', message);
+
+
+//         io.emit('newMessage', message);
+//     });
+
+//     socket.on('disconnect', () => {
+//         console.log('Client disconnected');
+//     });
+// });
+
+// server.listen(PORT, () => {
+//     console.log(`Server running on port ${PORT}`);
+// });
+
+
+
